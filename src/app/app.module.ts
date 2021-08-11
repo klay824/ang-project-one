fd65ad55d1ae944d0a762ede5163b3718535be25
klay824 <kchadwell0226@gmail.com>
2021-08-10 22:02:14 -0500

    learning two way binding, as well as directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -19,6 +20,7 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = ' ';
+  serverCreated = false;
+  servers = ['Testserver', 'testserver 2'];
+
+  // assignment 2
+  userName = ' ';
+  userNameCreationStatus = 'No username yet!';
+
+  // assignment 3
+  showSecret = false;
+  log = [];
 
   constructor() {
     setTimeout(() => {
@@ -17,4 +29,28 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  // assignment 2
+  onCreateUsername() {
+    this.userNameCreationStatus = 'Username was created and it is ' + this.userName;
+  }
+
+  onUpdateUsername(event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
+
+  // assignment 3
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length) + 1;
+  }
 }
